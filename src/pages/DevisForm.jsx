a8a68abbf048a002bdfe0ec080aeed8bab8a9e86
reddenconnexion <rotvener97@gmail.com>
2026-02-07 11:31:46 +0100

    fix(build): Remove import of non-existent PublicQuoteLink component

--- src/pages/DevisForm.jsx
+++ src/pages/DevisForm.jsx
@@ -20,7 +20,6 @@ import { getCoordinates, calculateDistance, getZoneFee } from '../utils/geoServi
 import PaymentSchedule from '../components/PaymentSchedule';
 import AmendmentFields from '../components/AmendmentFields'; // New Component
 import { useAutoSave, getDraft } from '../hooks/useAutoSave';
-import PublicQuoteLink from '../components/PublicQuoteLink';
 import SituationModal from '../components/SituationModal';
 
 const DevisForm = () => {
